2f22d1062f3433da6bbbef45665bb189f20b4c6e
weiiseli <132556039+weiiseli@users.noreply.github.com>
2024-04-28 17:19:56 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'53a8224f-d690-44d1-af81-3ad0fa1ce4a7'}
+UUID=${UUID:-'42f06e02-55ee-4317-9f6c-d3e794b66f41'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
